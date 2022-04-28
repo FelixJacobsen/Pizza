5c57e547e55bddc1d104814565e217dc2b424b10
Felix Jacobsen <felix.jacobsen@iths.se>
2022-04-28 18:26:12 +0200

    Add save function

--- src/main/java/se/iths/controller/PizzaController.java
+++ src/main/java/se/iths/controller/PizzaController.java
@@ -1,12 +1,18 @@
 package se.iths.controller;
 
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.http.HttpStatus;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
+import io.micronaut.http.annotation.Status;
 import io.micronaut.scheduling.TaskExecutors;
 import io.micronaut.scheduling.annotation.ExecuteOn;
 import se.iths.entity.Pizza;
 import se.iths.repository.PizzaRepository;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Controller("/pizzas")
@@ -23,6 +29,10 @@ public class PizzaController {
         return service.getAll();
     }
 
-    
+    @Post
+    @Status(HttpStatus.CREATED)
+    void save(@NonNull @NotNull @Valid Pizza pizza){
+        service.save(pizza);
+    }
 
 }
