e20a63f51221a3bde6aff503056e27f196f40b0b
FelixJacobsen <FelixJacobsen>
2022-04-28 15:38:47 +0000

    Initial commit

---
+++ src/test/java/se/iths/PizzaTest.java
@@ -0,0 +1,21 @@
+package se.iths;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class PizzaTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
