c10917e143b3954d25563b52052bb00c1fdfd89b
Felix Jacobsen <felix.jacobsen@iths.se>
2022-04-29 13:29:01 +0200

    Add Mongo database configuration interface

---
+++ src/main/java/se/iths/mongodb/MongoDbConfiguration.java
@@ -0,0 +1,11 @@
+package se.iths.mongodb;
+
+import io.micronaut.context.annotation.ConfigurationProperties;
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.core.naming.Named;
+@ConfigurationProperties("db")
+public interface MongoDbConfiguration extends Named {
+
+    @NonNull
+    String getCollection();
+}
